5359a5906472aa1aaee9945c5ae4651f0a72d9b9
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-09-16 16:00:41 -0400

    Multicolored triangle with position and color attribs in one VBO

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,21 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
-  "out vec4 vertexColor;\n"
+  "layout (location = 1) in vec3 aColor;\n"
+  "out vec3 ourColor;\n"
   "void main()\n"
   "{\n"
   " gl_Position = vec4(aPos, 1.0);\n"
-  " vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+  " ourColor = aColor;\n"
   "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
   "out vec4 FragColor;\n"
-  "uniform vec4 ourColor;\n"
+  "in vec3 ourColor;\n"
   "void main()\n"
   "{\n"
-  " FragColor = ourColor;\n"
+  " FragColor = vec4(ourColor, 1.0f);\n"
   "}\0";
-
 
 // Callback function to adjust the viewport if the GLFW window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -83,15 +83,18 @@ int main()
 
   // 3 vertices x, y, z 
   float vertices[] = {
-     0.0f,  0.5f, 0.0f, // top right 
-    -0.5f, -0.5f, 0.0f, // bottom right 
-     0.5f, -0.5f, 0.0f // bottom left 
+    // Positions       // Colors  
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left 
+    0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // top
   };
 
+  /* EBO indices
   unsigned int indices[] = { // note that we start from 0! 
     0, 1, 3, // first triangle
     1, 2, 3
   };
+  */
 
   // VAO
   unsigned int VAO;
@@ -114,8 +117,12 @@ int main()
   */
 
   // Linking Vertex Attributes
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  // Position attributes 
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  // Color attributes
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // Vertex shader 
   unsigned int vertexShader;
